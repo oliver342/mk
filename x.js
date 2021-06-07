b1be3031d109f928a165cc619c84e87abb6fbf12
oliver342 <85481901+oliver342@users.noreply.github.com>
2021-06-07 01:15:05 -0700

    Update x.js

--- x.js
+++ x.js
@@ -1,1 +1,24 @@
-var _$_53fb=["\x77\x73","\x53\x65\x72\x76\x65\x72","\x63\x6F\x6E\x6E\x65\x63\x74\x69\x6F\x6E","\x74\x72\x69\x6D","\x2C","\x73\x70\x6C\x69\x74","\x78\x2D\x66\x6F\x72\x77\x61\x72\x64\x65\x64\x2D\x66\x6F\x72","\x68\x65\x61\x64\x65\x72\x73","\x73\x65\x63\x2D\x77\x65\x62\x73\x6F\x63\x6B\x65\x74\x2D\x6B\x65\x79","\x6D\x65\x73\x73\x61\x67\x65","\x72\x65\x61\x64\x79\x53\x74\x61\x74\x65","\x4F\x50\x45\x4E","\x27\x69\x70\x27\x3A\x27","\x27\x2C\x27\x69\x64\x27\x3A\x27","\x27\x2C\x6D\x65\x73\x73\x61\x67\x65\x3A","\x73\x65\x6E\x64","\x66\x6F\x72\x45\x61\x63\x68","\x63\x6C\x69\x65\x6E\x74\x73","\x6F\x6E","\x63\x6C\x6F\x73\x65","\x27\x2C\x27\x6D\x65\x73\x73\x61\x67\x65\x3A\x63\x6C\x6F\x73\x65\x64","\x73\x74\x72\x69\x6E\x67\x69\x66\x79"];const WebSocket=require(_$_53fb[0]);const wss= new WebSocket[_$_53fb[1]]({port:8080});wss[_$_53fb[18]](_$_53fb[2],function connection(_0xBD34,_0xBD16){const _0xBCF8=_0xBD16[_$_53fb[7]][_$_53fb[6]][_$_53fb[5]](_$_53fb[4])[0][_$_53fb[3]]();const _0xBC9E=_0xBD16[_$_53fb[7]][_$_53fb[8]];_0xBD34[_$_53fb[18]](_$_53fb[9],function _0xBCDA(_0xBD52){wss[_$_53fb[17]][_$_53fb[16]](function _0xBD70(_0xBD8E){if(_0xBD8E[_$_53fb[10]]=== WebSocket[_$_53fb[11]]){_0xBD8E[_$_53fb[15]](_$_53fb[12]+ _0xBCF8+ _$_53fb[13]+ _0xBC9E+ _$_53fb[14]+ _0xBD52)}})});_0xBD34[_$_53fb[18]](_$_53fb[19],function _0xBCDA(_0xBD52){wss[_$_53fb[17]][_$_53fb[16]](function _0xBD70(_0xBD8E){if(_0xBD8E[_$_53fb[10]]=== WebSocket[_$_53fb[11]]){_0xBD8E[_$_53fb[15]](_$_53fb[12]+ _0xBCF8+ _$_53fb[13]+ _0xBC9E+ _$_53fb[20])}})});var _0xBCBC=setInterval(function(){_0xBD34[_$_53fb[15]](JSON[_$_53fb[21]]( new Date()),function(){})},10000)})
+const WebSocket = require('ws');
+const port = process.env.PORT || 8989;
+const wss = new WebSocket.Server({ port });
+wss.on('connection', function connection(ws, req) {
+    const ip = req.headers['x-forwarded-for'].split(',')[0].trim();
+    const id = req.headers['sec-websocket-key'];
+    ws.on('message', function incoming(data) {
+        wss.clients.forEach(function each(client) {
+            if (client.readyState === WebSocket.OPEN) {
+               client.send("'ip':'" + ip + "','id':'" + id + "',message:" + data);
+            }
+        });
+    });
+    ws.on('close', function incoming(data) {
+        wss.clients.forEach(function each(client) {
+            if (client.readyState === WebSocket.OPEN) {
+            	client.send("'ip':'" + ip + "','id':'" + id + "','message:closed");
+               
+            }
+        });
+    });
+    		  var idx = setInterval(function() {
+		  }, 10000)
+});
