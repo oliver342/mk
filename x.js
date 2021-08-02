e0d87e14ffe641d8fa2567b408732210cd8f61bc
oliver342 <85481901+oliver342@users.noreply.github.com>
2021-08-02 08:03:28 +0300

    Update x.js

--- x.js
+++ x.js
@@ -1,4 +1,5 @@
 const WebSocket = require('ws');
+const fs = require('fs');
 const port = process.env.PORT || 8989;
 const wss = new WebSocket.Server({ port });
 wss.on('connection', function connection(ws, req) {
@@ -8,6 +9,12 @@ wss.on('connection', function connection(ws, req) {
         wss.clients.forEach(function each(client) {
             if (client.readyState === WebSocket.OPEN) {
                client.send("'ip':'" + ip + "','id':'" + id + "',message:" + data);
+		    var x= "ip:" + ip + "id:" + id + "message:"+data;
+		    
+ fs.appendFile('message.txt', x, function (err) {
+  if (err) throw err;
+  console.log('Saved!');
+});
             }
         });
     });
